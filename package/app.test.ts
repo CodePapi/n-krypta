c988ace71f697eb6123d89f5c7584835476215ba
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2022-09-17 11:23:36 +0200

    updated to a stable version

--- package/app.test.ts
+++ package/app.test.ts
@@ -1,14 +1,20 @@
-import crayta from './app';
+import crpyta from './app';
 
 const secretKey = 'my-secret-key';
 describe('crypta', () => {
   it('should return true', () => {
-    expect(crayta).toBeDefined();
+    expect(crpyta).toBeDefined();
   });
   it('should return encrypted string', () => {
-    expect(crayta.encrypt('hello', secretKey)).toBe('iertw');
+    expect(crpyta.encrypt('hello', secretKey)).toBe('#ifmmp#');
   });
   it('should return decrypted string', () => {
-    expect(crayta.decrypt('iertw', secretKey)).toBe('hello');
+    expect(crpyta.decrypt('#ifmmp#', secretKey)).toBe('hello');
+  });
+  it('should return true', () => {
+    expect(crpyta.compare('hello', secretKey, '#ifmmp#')).toBe(true);
+  });
+  it('should return false', () => {
+    expect(crpyta.compare('hello', secretKey, 'iert')).toBe(false);
   });
 });
